859cf5d6076d5aa363e350972f4f96dc45fcd7d7
yusuke-97 <yankeesyuu@gmail.com>
2026-06-05 22:33:43 +0900

    GTMの修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP, Roboto } from "next/font/google";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { SiteFooter } from "../components/SiteFooter";
 import { SiteHeader } from "../components/SiteHeader";
 import {
@@ -71,27 +71,9 @@ export default function RootLayout({
       className={`${roboto.variable} ${notoSansJp.variable} h-full antialiased`}
     >
       {isProduction && (
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `}
-        </Script>
+        <GoogleTagManager gtmId={gtmId} />
       )}
       <body className="min-h-full flex flex-col">
-        {isProduction && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        )}
         <SiteHeader />
         <div className="flex-1">{children}</div>
         <SiteFooter />
